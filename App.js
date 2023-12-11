96ad93b21854f49a7dbf75e501e9f3c3c4535bea
Isaac Adumoah <sduisaac@yahoo.com>
2023-12-11 23:46:11 +0000

    initial

--- App.js
+++ App.js
@@ -1,20 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { Button, Modal, View } from 'react-native';
 
-export default function App() {
+function App() {
+  const [isModalVisible,setIsModalVisible] = useState(false);
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex:1,backgroundColor:'plum'}}>
+      <Button title='Press' onPress={()=>setIsModalVisible(true)}
+      />
+      <Modal visible={isModalVisible}
+      animationType='slide'
+      >
+        <View style={{backgroundColor:'midnightblue'}}>
+          <Button title='Modal Content'color={'midnightblue'} onPress={()=>setIsModalVisible(false)}
+          
+          />
+        </View>
+      </Modal>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
